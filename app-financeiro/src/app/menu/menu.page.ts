3fb97f5a742d2e491047dde4b0ee17f331dc4819
FrugoniMatheus <frugoni.kauan@gmail.com>
2026-04-02 16:31:55 -0300

    Update menu, add 3 routers receber, pagar, cadastro

--- app-financeiro/src/app/menu/menu.page.ts
+++ app-financeiro/src/app/menu/menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  cadastro(){
+    this.router.navigate(['/cadastro'])
+  }
+  contasAReceber(){
+    this.router.navigate(['/receber'])
+  }
+  contasAPagar(){
+    this.router.navigate(['/pagar'])
   }
 
 }
